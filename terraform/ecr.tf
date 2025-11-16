8513bb91628d09851477e47e0970a79760af174c
Mateusz Nasewicz <mateusznasewicz@gmail.com>
2025-11-16 17:21:04 +0100

    git ignore upd

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "frontend_repo" {
     name = var.frontend_repo
+    force_delete = true
 
     tags = {
         Name = "${var.app_name}-frontend-ecr"
@@ -8,6 +9,7 @@ resource "aws_ecr_repository" "frontend_repo" {
 
 resource "aws_ecr_repository" "backend_repo" {
     name = var.backend_repo
+    force_delete = true
 
     tags = {
         Name = "${var.app_name}-backend-ecr"
